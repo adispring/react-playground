ea50485d6d9e9c383accaa7f2cb2330e26ab43ef
wangzengdi <sunnyadi@163.com>
2018-04-24 11:37:01 +0800

    React Virtual Dom Diff

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const childs = [['A', 'B', 'C', 'D'], ['B', 'A', 'D', 'C']];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      childs,
+      index: 0,
+    };
+  }
+  onClick = () => {
+    const index = (this.state.index + 1) % 2;
+    this.setState({ index });
+  };
+  render() {
+    const { childs, index } = this.state;
+    console.log(index);
+    return (
+      <div className="App" onClick={this.onClick}>
+        {childs[index].map((val, i) => <div key={val}>{val}</div>)}
+      </div>
+    );
+  }
+}
+
+export default App;
